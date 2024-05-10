0a7a414b5d0c9f90f2c06c2fc120b115ff5131d1
Baptiste Durand <ba.durand50@gmail.com>
2024-05-11 00:49:18 +0200

    Minor changes

--- common.mk
+++ common.mk
@@ -4,7 +4,6 @@
 
 # Defining top directory
 TOP_DIR := $(realpath .)
-TB_TEMPLATE_DIR := $(realpath tb/template)
 
 # Generic RM command
 RM := rm -f
--- common.py
+++ common.py
@@ -2,6 +2,7 @@
 #  Author : Baptiste Durand  #
 # ========================== #
 
+# import re library
 import re
 
 
